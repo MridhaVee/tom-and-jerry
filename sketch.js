9824219240f9911e2f756aebb8c61b2a02b22168
WhiteHatProdigy <78709166+WhiteHatProdigy@users.noreply.github.com>
2021-03-06 21:09:23 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,13 +3,13 @@ var mouse,mouseImg,mouseImg2,mouse_moving
 var garden, gardenImg
 function preload() {
     //load the images here
-gardenImg=loadImage("garden.png")
-catImg=loadImage("cat1.png")
-catImg2=loadImage("cat4.png")
-cat_moving=loadAnimation("cat2.png","cat3.png")
-mouseImg=loadImage("mouse1.png")
-mouseImg2=loadImage("mouse4.png")
-mouse_moving=loadAnimation("mouse2.png","mouse3.png")
+gardenImg=loadImage("images/garden.png")
+catImg=loadAnimation("images/cat1.png")
+catImg2=loadAnimation("images/cat4.png")
+cat_moving=loadAnimation("images/cat2.png","images/cat3.png")
+mouseImg=loadAnimation("images/mouse1.png")
+mouseImg2=loadAnimation("images/mouse4.png")
+mouse_moving=loadAnimation("images/mouse2.png","images/mouse3.png")
 }
 
 function setup(){
@@ -18,12 +18,42 @@ function setup(){
     garden=createSprite(500,400,20,20)
     garden.addImage(gardenImg)
 
-}
+    cat = createSprite(700,660)
+    cat.addAnimation("stand",catImg);
+    cat.scale = 0.1;
+  
+    mouse = createSprite(100,660)
+    mouse.addAnimation("first",mouseImg);
+    mouse.scale = 0.1;
+  
+  }
+
+
 
 function draw() {
 
     background(255);
     //Write condition here to evalute if tom and jerry collide
+
+    if(keyDown("LEFT_ARROW")){
+
+        cat.velocityX = -5;
+        cat.addAnimation("moving",cat_moving);
+        cat.changeAnimation("moving");
+        mouse.addAnimation("mouse",mouse_moving);
+        mouse.changeAnimation("mouse");
+        
+        }
+        
+        if(cat.isTouching(mouse)){
+        
+            cat.velocityX = 0;
+            mouse.addAnimation("mouseI",mouseImg2);
+            mouse.changeAnimation("mouseI");
+            cat.addAnimation("catworry",catImg2);
+            cat.changeAnimation("catworry");
+        }
+        
 
     drawSprites();
 }
